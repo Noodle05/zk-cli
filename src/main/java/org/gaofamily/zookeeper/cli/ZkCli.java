48afb8f11875549581f239985957d171aeaa634a
Wei Gao <wgao@salesforce.com>
2017-02-19 20:34:06 -0800

    first commit

---
+++ src/main/java/org/gaofamily/zookeeper/cli/ZkCli.java
@@ -0,0 +1,186 @@
+package org.gaofamily.zookeeper.cli;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+import org.kohsuke.args4j.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+/**
+ * @author Wei Gao
+ * @since 2/18/17
+ */
+public class ZkCli {
+    private static final Logger logger = LoggerFactory.getLogger(ZkCli.class);
+
+    private static final int SESSION_TIMEOUT = 1000;
+    private static final String PATH_SEPARATOR = "/";
+
+    @Option(name = "-s")
+    private String source;
+
+    @Option(name = "-d")
+    private String dest;
+
+    void syncFolder(final Path source, final String dest, final ZooKeeper zkClient,
+                    Consumer<String> callback, Consumer<Map<String, Throwable>> exceptionHandler) {
+        try {
+            logger.debug("Syncing folder from: {} to {} on zookeeper: {}", source, dest, zkClient);
+            assert Files.isDirectory(source);
+            final Path folderName = source.getFileName();
+            final String fDest = (dest.endsWith(PATH_SEPARATOR) ? dest : dest + PATH_SEPARATOR) + folderName.toString();
+            final byte[] data = new byte[0];
+            putData(zkClient, fDest, data, path -> {
+                logger.trace("Put data success for path: {}", path);
+                try {
+                    AtomicInteger counter = new AtomicInteger(0);
+                    counter.incrementAndGet();
+                    final Map<String, Throwable> exceptions = new HashMap<>();
+                    Files.list(source).forEach(file -> {
+                        logger.trace("List though souce: {}, get file: {}", source, file);
+                        counter.incrementAndGet();
+                        if (Files.isRegularFile(file)) {
+                            syncFile(file, path, zkClient, name -> {
+                                countDown(path, callback, exceptionHandler, exceptions, counter);
+                            }, exp -> {
+                                exceptions.put(path, exp);
+                                countDown(path, callback, exceptionHandler, exceptions, counter);
+                            });
+                        } else if (Files.isDirectory(file)) {
+                            syncFolder(file, path, zkClient, name -> {
+                                countDown(path, callback, exceptionHandler, exceptions, counter);
+                            }, exps -> {
+                                exceptions.putAll(exps);
+                                countDown(path, callback, exceptionHandler, exceptions, counter);
+                            });
+                        }
+                    });
+                    countDown(path, callback, exceptionHandler, exceptions, counter);
+                } catch (Throwable e) {
+                    exceptionHandler.accept(Collections.singletonMap(path, e));
+                }
+            }, exp -> {
+                exceptionHandler.accept(Collections.singletonMap(fDest, exp));
+            });
+        } catch (Throwable e) {
+            exceptionHandler.accept(Collections.singletonMap(dest, e));
+        }
+    }
+
+    void syncFile(Path source, String dest, ZooKeeper zkClient,
+                  Consumer<String> callback, Consumer<Throwable> exceptionHandler) {
+        try {
+            logger.debug("Put file from: {} to {} on zookeeper: {}", source, dest, zkClient);
+            assert Files.isRegularFile(source);
+            Path folderName = source.getFileName();
+            final String fDest = (dest.endsWith(PATH_SEPARATOR) ? dest : dest + PATH_SEPARATOR) + folderName.toString();
+            final byte[] data = Files.readAllBytes(source);
+            putData(zkClient, fDest, data, name -> callback.accept(name), exp -> exceptionHandler.accept(exp));
+        } catch (Throwable e) {
+            if (exceptionHandler != null) {
+                exceptionHandler.accept(e);
+            }
+        }
+    }
+
+    private void countDown(String dest, Consumer<String> callback, Consumer<Map<String, Throwable>> exceptionHandler,
+                           Map<String, Throwable> exceptions, AtomicInteger counter) {
+        if (counter.decrementAndGet() == 0) {
+            if (exceptions != null && !exceptions.isEmpty()) {
+                if (exceptionHandler != null) {
+                    exceptionHandler.accept(exceptions);
+                }
+            } else {
+                if (callback != null) {
+                    callback.accept(dest);
+                }
+            }
+        }
+    }
+
+    void putData(ZooKeeper zkClient, final String dest, byte[] data,
+                 Consumer<String> callback, Consumer<Throwable> exceptionHandler) {
+        try {
+            logger.trace("Putting {} bytes to {} on zookeeper: {}", data.length, dest, zkClient);
+            zkClient.exists(dest, false, (rc, path, ctx, state) -> {
+                logger.trace("Check if {} exists return code: {}", path, rc);
+                KeeperException.Code code = KeeperException.Code.get(rc);
+                logger.trace("Return code: {} map to: {}", rc, code);
+                switch (code) {
+                    case OK:
+                        logger.trace("Found node on {}, update data.", path);
+                        zkClient.setData(path, data, state.getVersion(), (nrc, npath, nctx, nstate) -> {
+                            logger.trace("Update data on node {} done, return code: {}", npath, nrc);
+                            KeeperException.Code ncode = KeeperException.Code.get(nrc);
+                            logger.trace("Result code: {} map to: {}", nrc, ncode);
+                            if (ncode.equals(KeeperException.Code.OK)) {
+                                if (callback != null) {
+                                    logger.trace("Find callback, calling callback with path: {}", npath);
+                                    callback.accept(npath);
+                                } else {
+                                    logger.trace("No callback found.s");
+                                }
+                            } else {
+                                logger.debug("Result code is not OK, throw exception");
+                                if (exceptionHandler != null) {
+                                    exceptionHandler.accept(KeeperException.create(nrc, npath));
+                                } else {
+                                    logger.trace("No exception handler found.");
+                                }
+                            }
+                        }, null);
+                        break;
+                    case NONODE:
+                        logger.trace("Didn't find node {}, create one.", path);
+                        zkClient.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT, (nrc, npath, nctx, nname) -> {
+                            logger.trace("Create node {} done, return code: {}", npath, nrc);
+                            KeeperException.Code ncode = KeeperException.Code.get(nrc);
+                            logger.trace("Result code: {} map to: {}", nrc, ncode);
+                            if (ncode.equals(KeeperException.Code.OK)) {
+                                if (callback != null) {
+                                    logger.trace("Find callback, calling callback with path: {}", npath);
+                                    callback.accept(npath);
+                                } else {
+                                    logger.trace("No callback found.");
+                                }
+                            } else {
+                                logger.debug("Result code is not OK, throw exception");
+                                if (exceptionHandler != null) {
+                                    exceptionHandler.accept(KeeperException.create(nrc, npath));
+                                } else {
+                                    logger.trace("No exception handler found.");
+                                }
+                            }
+                        }, null);
+                        break;
+                    default:
+                        logger.debug("Do not recoganize result code, throw exception");
+                        if (exceptionHandler != null) {
+                            exceptionHandler.accept(KeeperException.create(rc, path));
+                        } else {
+                            logger.trace("No exception handler found.");
+                        }
+                }
+            }, null);
+        } catch (Throwable e) {
+            if (exceptionHandler != null) {
+                exceptionHandler.accept(e);
+            } else {
+                logger.trace("No exception handler found.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/org/gaofamily/zookeeper/cli/ZkCliTest.java
@@ -0,0 +1,148 @@
+package org.gaofamily.zookeeper.cli;
+
+import org.apache.curator.test.TestingServer;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author Wei Gao
+ * @since 2/18/17
+ */
+public class ZkCliTest implements Watcher {
+    private static final Logger logger = LoggerFactory.getLogger(ZkCliTest.class);
+
+    private TestingServer zkTestServer;
+
+    @BeforeTest
+    public void startZookeeper() throws Exception {
+        logger.info("Starting zookeeper");
+        zkTestServer = new TestingServer();
+        logger.info("Zookeeper connect string: {}", zkTestServer.getConnectString());
+    }
+
+    @Test(groups = "unit")
+    public void testSingleFile() throws InterruptedException, IOException {
+        ZkCli zkCli = new ZkCli();
+        logger.debug("Creating temporary file");
+        Path tmpFile = Files.createTempFile("Test", "_data");
+        ZooKeeper zkClient = new ZooKeeper(zkTestServer.getConnectString(), 1000, this);
+        try {
+            List<String> lines = new ArrayList<>(10);
+            for (int i = 0; i < 10; i++) {
+                lines.add(UUID.randomUUID().toString());
+            }
+            final Lock lock = new ReentrantLock();
+            final Condition condition = lock.newCondition();
+            lock.lock();
+            try {
+                Files.write(tmpFile, lines);
+                AtomicBoolean synced = new AtomicBoolean(false);
+                zkCli.syncFile(tmpFile, "/", zkClient, name -> {
+                    logger.debug("Sync single file to {} done.", name);
+                    lock.lock();
+                    try {
+                        synced.compareAndSet(false, true);
+                        condition.signal();
+                    } finally {
+                        lock.unlock();
+                    }
+                }, exp -> {
+                    lock.lock();
+                    try {
+                        synced.compareAndSet(false, true);
+                        condition.signal();
+                    } finally {
+                        lock.unlock();
+                    }
+                    Assert.fail("Failed to copy file", exp);
+                });
+                if (!synced.get()) {
+                    condition.await();
+                }
+            } finally {
+                lock.unlock();
+            }
+        } finally {
+            zkClient.close();
+            logger.debug("Deleteing temporary file: {}", tmpFile);
+            Files.delete(tmpFile);
+        }
+    }
+
+    @Test(groups = "unit")
+    public void testFolder() throws InterruptedException, IOException {
+        ZkCli zkCli = new ZkCli();
+        Path folder = Paths.get("/Users/wgao/work/trunk/dc_config/dc_config_djsolr/config");
+        final Lock lock = new ReentrantLock();
+        final Condition condition = lock.newCondition();
+        ZooKeeper zkClient = new ZooKeeper(zkTestServer.getConnectString(), 1000, this);
+        final Map<String, Throwable> exceptions = new HashMap<>();
+        lock.lock();
+        try {
+            AtomicBoolean synced = new AtomicBoolean(false);
+            zkCli.syncFolder(folder, "/", zkClient, name -> {
+                lock.lock();
+                try {
+                    synced.compareAndSet(false, true);
+                    condition.signal();
+                } finally {
+                    lock.unlock();
+                }
+            }, exps -> {
+                exceptions.putAll(exps);
+                lock.lock();
+                try {
+                    synced.compareAndSet(false, true);
+                    condition.signal();
+                } finally {
+                    lock.unlock();
+                }
+            });
+            if (!synced.get()) {
+                condition.await();
+            }
+            if (!exceptions.isEmpty()) {
+                logger.error("Copy folder failed with {} enties.", exceptions.size());
+                exceptions.forEach((np, exp) -> {
+                    logger.debug("Path: {} got exception.", np, exp);
+                });
+                Assert.fail("Copy folder failed.");
+            }
+        } finally {
+            zkClient.close();
+            lock.unlock();
+        }
+    }
+
+    @AfterTest
+    public void stopZookeeper() throws IOException, InterruptedException {
+        logger.info("Stopping zookeeper");
+        zkTestServer.stop();
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        logger.info("Get watched event: {}", event);
+    }
+}
